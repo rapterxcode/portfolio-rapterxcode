88a6ea6da4cf6d555db17827f373f22cccea9f6d
rapterxcode <phonepasit9@gmail.com>
2025-06-24 20:23:43 +0700

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import './App.css'
 import App from './App.tsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+], {
+  basename: '/portfolio-rapterxcode'
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
